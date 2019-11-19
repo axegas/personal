df7ac8fe6663394f6d2a53074f86a3977496bede
axegas <axegas@alu.iesserpis.org>
2019-11-19 17:55:26 +0100

    ty

--- Boletin 1/src/Tres.java
+++ Boletin 1/src/Tres.java
@@ -7,6 +7,7 @@ public class Tres {
 	static char[][] tablero = new char[3][3];
 	static int[][] jugador = new int[3][3];
 	static int[][] maquina = new int[3][3];
+	
 	public static void main(String[] args) {
 		boolean victoria = false;
 		for(int i=0;i<3;i++) {
@@ -59,8 +60,8 @@ public class Tres {
 		int x;
 		int y;
 		do {
-			x = getNumeroRandom(0,2);
-			y = getNumeroRandom(0,2);			
+			x = getIntRandom(0,2);
+			y = getIntRandom(0,2);			
 		}while(m[x][y]!='-');
 		m[x][y]='0';
 		maquina[x][y]=1;
@@ -75,16 +76,17 @@ public class Tres {
 		}
 	
 	public static boolean cabeValor(char[][] m) {
-		boolean cabe = false;
+		
 		for(int i=0;i<3;i++) {
 			for(int j=0;j<3;j++) {
 				if(m[i][j]=='-')
-					cabe = true;
+					return true;
 			}
 		}
-		return cabe;
+		return false;
 		}
-	public static int getNumeroRandom(int min, int max){
+	
+	public static int getIntRandom(int min, int max){
 		return ThreadLocalRandom.current().nextInt(min, max+1);
 	}
 	
@@ -92,29 +94,25 @@ public class Tres {
 		
 		int sumaFila = 0;
 		int sumaCol = 0;
-		int sumaDig1 = 0;
-		int sumaDig2 = 0;
+		int sumaDiag1 = 0;
+		int sumaDiag2 = 0;
 		for(int i=0;i<3;i++) {
 			for(int j=0;j<3;j++) {
 				sumaFila += num[i][j];
 				sumaCol += num[j][i];
+				sumaDiag1 += num[j][j];
+				sumaDiag2 += num[j][2-j];
 			}
-			if(sumaFila==3 || sumaCol==3) {
+			if(sumaFila==3 || sumaCol==3 || sumaDiag1==3 || sumaDiag2==3) {
 				return true;
 			}else {
 				sumaFila=0;
 				sumaCol = 0;
+				sumaDiag1 = 0;
+				sumaDiag2 = 0;
 			}
 		}
-		for(int i=0;i<3;i++) {
-			sumaDig1 += num[i][i];
-			sumaDig2 += num[i][2-i];
-		}
-		if(sumaDig1==3 || sumaDig2==3) {
-			return true;
-		}else {
-			return false;
-		}
+		return false;		
 	}
 
 }
