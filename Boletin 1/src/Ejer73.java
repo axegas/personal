57bf3fa0627a91e08960f7621b7fe123ea3f9194
axegas <axegas@alu.iesserpis.org>
2019-11-27 18:52:09 +0100

    dfhs

--- Boletin 1/src/Ejer73.java
+++ Boletin 1/src/Ejer73.java
@@ -5,14 +5,44 @@ public class Ejer73 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);		
 		String transmision;
+		String transIni = "";
+		int j=0;
+		int i=0;
 		
 		System.out.print("Introduce la transmisión a cifrar: ");
 		transmision = scan.nextLine();
+		
+		/*
 		if(!transmision.contains("el area 51")) {
 			System.out.println("'el area 51' no aparece en la transmisión");
 		}else {
 			transmision = transmision.replaceAll("el area 51", "Murcia");
 			System.out.println(transmision);				
 		}
+		*/
+		if(!transmision.contains("el area 51")) {
+			System.out.println("'el area 51' no aparece en la transmisión");
+		}else {
+			i = transmision.indexOf("el area 51");
+			while(true){
+				if(!transmision.contains("el area 51")) {
+					transIni += transmision.substring(j, transmision.length());
+					break;
+				}
+				else {
+					transIni += transmision.substring(j, i);
+					transIni += "Murcia";
+					transmision = transmision.substring(i+"el area 51".length(),transmision.length());	
+					i = transmision.indexOf("el area 51");
+					
+				}
+				
+			}
+			System.out.println(transIni);
+		}
+		
+		
+		
 	}
+	
 }
