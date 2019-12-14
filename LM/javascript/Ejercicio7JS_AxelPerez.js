ae73256828297e866b803974c1db4c011c8dbcba
axegas <axegas@alu.iesserpis.org>
2019-12-14 14:37:56 +0100

    fgs

--- LM/javascript/Ejercicio7JS_AxelPerez.js
+++ LM/javascript/Ejercicio7JS_AxelPerez.js
@@ -1,104 +1,59 @@
 /*
-var operandoa;
-var operandob;
-var operacion;
-var operando = 0;
-function init(){
-    //variables
-    var resultado = document.getElementById('resultado');
-    var reset = document.getElementById('reset');
-    var suma = document.getElementById('suma');
-    var resta = document.getElementById('resta');
-    var multiplicacion = document.getElementById('multiplicacion');
-    var division = document.getElementById('division');
-    var igual = document.getElementById('igual');
-    var uno = document.getElementById('uno');
-    var dos = document.getElementById('dos');
-    var tres = document.getElementById('tres');
-    var cuatro = document.getElementById('cuatro');
-    var cinco = document.getElementById('cinco');
-    var seis = document.getElementById('seis');
-    var siete = document.getElementById('siete');
-    var ocho = document.getElementById('ocho');
-    var nueve = document.getElementById('nueve');
-    var cero = document.getElementById('cero');
-    var coma = document.getElementById('coma');
-    
-    //Eventos de click
-    coma.onclick = function(e){
-        resultado.textContent = resultado.textContent  + ",";
-    }
-    uno.onclick = function(e){
-        resultado.textContent = resultado.textContent  + "1";
-    }
-    dos.onclick = function(e){
-        resultado.textContent = resultado.textContent  + "2";
-    }
-    tres.onclick = function(e){
-        resultado.textContent = resultado.textContent  + "3";
-    }
-    cuatro.onclick = function(e){
-        resultado.textContent = resultado.textContent  + "4";
-    }
-    cinco.onclick = function(e){
-        resultado.textContent = resultado.textContent  + "5";
-    }
-    seis.onclick = function(e){
-        resultado.textContent = resultado.textContent  + "6";
-    }
-    siete.onclick = function(e){
-        resultado.textContent = resultado.textContent  + "7";
-    }
-    ocho.onclick = function(e){
-        resultado.textContent = resultado.textContent  + "8";
-    }
-    nueve.onclick = function(e){
-        resultado.textContent = resultado.textContent  + "9";
-    }
-    cero.onclick = function(e){
-        resultado.textContent = resultado.textContent  + "0";
-    }
-    reset.onclick = function(e){
-        resetear();
-    }
-    suma.onclick = function(e){
-        operandoa = resultado.textContent;
-        operacion = "+";
+CALCULADORA:
+Operaciones implementadas:
+    - Sumar
+    - Restar
+    - Multiplicar
+    - Dividir
+    - Raiz cuadrada
+    - Elevar a un numero
+*/
+var res;
+var operandoa = "";
+var operandob = "";
+var operacion = "";
+var resuelto = "";
+var tiene = false;
+var prueba = "";
+function ponNumero(num){
+    if(resuelto){
         limpiar();
+        resuelto=false;
     }
-    resta.onclick = function(e){
-        operandoa = resultado.textContent;
-        operacion = "-";
-        limpiar();
+    resultado.textContent = resultado.textContent + num;       
+}
+function ponComa(coma){
+    prueba = resultado.textContent;
+    for(i=0;i<prueba.length;i++){
+        if(prueba.charAt(i)=="."){
+            tiene=true;
+            break;
+        }
     }
-    multiplicacion.onclick = function(e){
-        operandoa = resultado.textContent;
-        operacion = "*";
-        limpiar();
-    }
-    division.onclick = function(e){
-        operandoa = resultado.textContent;
-        operacion = "/";
-        limpiar();
-    }
-    igual.onclick = function(e){
-        operandob = resultado.textContent;
-        resolver();
-    }
+    if(!tiene){
+        if(resuelto){
+            limpiar();
+            resuelto=false;
+        }
+        resultado.textContent = resultado.textContent + coma; 
+    }    
 }
 
-function limpiar(){
-    resultado.textContent = "";
+function opera(op){
+    operandoa = resultado.textContent;
+    operacion = "" + op;
+    limpiar();
+    tiene=false;
 }
-function resetear(){
-    resultado.textContent = "";
-    operandoa = 0;
-    operandob = 0;
-    operacion = "";
+function eleva(){
+    operandoa = resultado.textContent;
+    limpiar();
+    tiene=false;
 }
 
-function resolver(){
-    var res = 0;
+function resolver(){ 
+    res = "";
+    operandob=resultado.textContent;
     switch(operacion){
         case "+":
             res = parseFloat(operandoa) + parseFloat(operandob);
@@ -111,78 +66,36 @@ function resolver(){
             break;
         case "/":
             res = parseFloat(operandoa) / parseFloat(operandob);
+            break;    
+        case "^":
+            res = Math.pow(parseFloat(operandoa),parseFloat(operandob));
             break;
-    }
-    resetear();
-    resultado.textContent = res;
-}
-
-*/
-var res;
-var operandoa = 0;
-var operandob;
-var operacion;
-var resultado;
-function ponNumero(num){
-    
-    //resultado.textContent = resultado.textContent + num; 
-    document.getElementById('resultado').innerHTML=resultado.textContent99999999999999999999999;
-    res = resultado.textContent;
-    window.alert(res);
-   /* window.alert(res);*/
-    
-}
-/*
-function divide(){
-    operandoa = res;
-    operacion = "/";
-    limpiar();
-}
-function multiplica(){
-    operandoa = res;
-    operacion = "*";
-    limpiar();
-}
-function suma(){
-    operandoa = res;
-    operacion = "+";
-    limpiar();
-}
-function resta(){
-    operandoa = res;
-    operacion = "-";
-    limpiar();
-}
-
-
-
-
-function resuelve(){    
-    if(operandoa!=0){
-        operandob=parseFloat(res);
-        switch(operacion){
-            case "+":
-                resultado = parseFloat(operandoa) + parseFloat(operandob);
-                break;
-            case "-":
-                resultado = parseFloat(operandoa) - parseFloat(operandob);
-                break;
-            case "*":
-                resultado = parseFloat(operandoa) * parseFloat(operandob);
-                break;
-            case "/":
-                resultado = parseFloat(operandoa) / parseFloat(operandob);
-                break;     
         }
-    }else{
-        operandoa = parseFloat(res);
-    }    
-    window.alert(resultado);
-    limpiar();    
+    resultado.textContent = res;  
+    operacion = "";
+    operandob = "";
+    resuelto=true;
+}
+
+function resuelveRaiz(){ 
+    res = "";
+    operandob=resultado.textContent;
+    num = parseFloat(operandob);
+    res = Math.sqrt(num);
+    resultado.textContent = res;  
+    operacion = "";
+    operandob = "";
+    resuelto=true;
 }
 
 function limpiar(){
     resultado.textContent = "";
 }
 
-*/
+function reset(){
+    resultado.textContent = "";
+    operandoa = "";
+    operandob = "";
+    res = "";
+    resuelto = "";
+}
