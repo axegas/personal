51c5845e308a5528482cf59b36b028f707c3ceba
axegas <axegas@alu.iesserpis.org>
2020-03-10 13:17:30 +0100

    fg

---
+++ objetos/src/Zoo/Animal.java
@@ -0,0 +1,27 @@
+package Zoo;
+
+public class Animal {
+	
+	private String tipo;
+	private String nombre;
+	private int peso;
+	
+	public Animal(String tipo, String nombre, int peso) {
+		this.nombre = nombre;
+		this.peso = peso;
+		if(tipo.equals("t")) {
+			this.tipo = "Tigre";
+		}else {
+			this.tipo = "Leon";
+		}
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
+	public String toString() {
+		return tipo + " de nombre " + nombre + " y peso " + peso + "Kg.\n";
+	}
+}
